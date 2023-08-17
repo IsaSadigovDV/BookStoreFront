688fbac5bd74784bdcebea692d11b7c351620241
IsaSadigovDV <isans@code.edu.az>
2023-08-17 17:20:47 +0400

    swiper pagination added

--- Js/swiper.js
+++ Js/swiper.js
@@ -1,6 +1,7 @@
 const swiper = new Swiper(".book-swiper", {
   slidesPerView: 2,
   spaceBetween: 10,
+  loop: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -16,8 +17,50 @@ const swiper = new Swiper(".book-swiper", {
 });
 
 const swiperCompany = new Swiper(".brand-swiper", {
-  slidesPerView: 4,
+  // slidesPerView: 4,
   spaceBetween: 10,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+
+    565: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+});
+
+const feedback = new Swiper(".feedback-swiper", {
+  slidesPerView: 1,
+  loop: true,
+
+  // spaceBetween: 10,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -31,3 +74,5 @@ const swiperCompany = new Swiper(".brand-swiper", {
     clickable: true,
   },
 });
+
+// var feedback = new Swiper(".feedback-swiper", {});

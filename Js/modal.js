8e0f4ae0948d883de0272845ab47004add11ffce
IsaSadigovDV <isans@code.edu.az>
2023-08-18 16:16:33 +0400

    single product modal added

--- Js/modal.js
+++ Js/modal.js
@@ -1,20 +1,22 @@
-// Get the modal and the button that opens it
-var modal = document.getElementById("cartModal");
-var addToCartButton = document.getElementById("addToCartButton");
+const modal = document.getElementById("cartModal");
+const addToCartButton = document.getElementById("addToCartButton");
+const closeIcon = document.querySelector(".closeIcon");
 
-// When the user clicks the "Add to Cart" button, open the modal
-addToCartButton.addEventListener("click", function() {
-    modal.style.display = "block";
+console.log(modal);
+console.log(addToCartButton);
+
+addToCartButton.addEventListener("click", function () {
+  modal.style.visibility = "visible";
+  modal.style.opacity = 1;
 });
 
-var closeIcon = document.querySelector(".modal-heading i");
-closeIcon.addEventListener("click", function() {
-    modal.style.display = "none";
+closeIcon.addEventListener("click", function () {
+  modal.style.visibility = "hidden";
+  modal.style.opacity = 0;
 });
 
-// When the user clicks anywhere outside of the modal, close it
-window.addEventListener("click", function(event) {
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
-});
+// window.addEventListener("click", function (event) {
+//   if (event.target === modal) {
+//     modal.style.display = "none";
+//   }
+// });
